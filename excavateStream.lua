c65a1d77311bcd1885658e63e9e824653fca1b0f
Dean Ralph <54666004+deanralph@users.noreply.github.com>
2025-02-21 08:27:26 +0000

    added some more functions

--- excavateStream.lua
+++ excavateStream.lua
@@ -7,14 +7,28 @@ facing = 1 -- 1: Forward, 2: Right, 3: Backwards, 4: Left
 
 -- Functions
 local fucntion printHeader()
+    term.clear()
+    term.setCursorPos(1, 1)
     print("-------------------------------")
     print("      ExavateStream 4031")
     print(" A DriftStream product 2025 TM")
     print("-------------------------------")
+    print()
 end
 
 local function getInput(prompt)
-    print(prompt)
+    local input
+    while true do
+        term.write(prompt .. " ")  -- Display the prompt
+        input = read()  -- Read user input
+
+        local number = tonumber(input)  -- Convert to number
+        if number and math.floor(number) == number then
+            return number  -- Return valid integer
+        else
+            print("Invalid input. Please enter a whole number.")
+        end
+    end
 end
 
 local function turnToFace(targetDirection)
@@ -41,16 +55,16 @@ local function digMoveForward()
         turtle.dig()
     end
     turtle.forward()
-    if facing == 0 
+    if facing == 1 
     then
         z = z + 1
-    elseif facing == 1 
-    then
-        x = x + 1
     elseif facing == 2 
     then
-        z = z - 1
+        x = x + 1
     elseif facing == 3 
+    then
+        z = z - 1
+    elseif facing == 4 
     then
         x = x - 1
     end
@@ -65,10 +79,60 @@ local function moveUp()
     y = y + 1
 end
 
--- Main code
+local function moveDown()
+    while turtle.detectDown() 
+    do
+        turtle.digDown()
+    end
+    turtle.down()
+    y = y - 1
+end
 
+local function moveToOrigin()
+    -- Move to X = 0
+    if x > 0 
+    then
+        turnToFace(4)
+        for _ = 1, x 
+        do 
+            digMoveForward() 
+        end
+    elseif x < 0 
+    then
+        turnToFace(2)
+        for _ = 1, -x 
+        do
+            digMoveForward()
+        end
+    end
+
+    -- Move to Z = 0
+    if z > 0 
+    then
+        turnToFace(3)
+        for _ = 1, z 
+        do 
+            digMoveForward()
+        end
+    elseif z < 0 
+    then
+        turnToFace(1)
+        for _ = 1, -z 
+        do 
+            digMoveForward() 
+        end
+    end
+
+    -- Face +Z direction
+    turnToFace(1)
+end
+
+-- Main code
 printHeader()
 printLocation()
 moveUp()
 printLocation()
 digMoveForward()
+printLocation()
+moveToOrigin()
+printLocation()
